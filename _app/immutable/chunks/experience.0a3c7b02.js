99fcddf2b1d52d60993c9237d963f21e02e0546f
riki-awan <153417904+riki-awan@users.noreply.github.com>
2024-09-12 04:42:21 +0000

    Deploying to gh-pages from @ riki-awan/porto@96673affcf38ce7bc4d74c279e7e9f25f9227ff2 🚀

---
+++ _app/immutable/chunks/experience.0a3c7b02.js
@@ -0,0 +1,8 @@
+import{A as e}from"./UIcon.b4252c75.js";import{g as n}from"./skills.f2c3a546.js";import{C as o}from"./types.1ebf2f37.js";const i=[{slug:"open-sourcer",company:"Staff IT",description:`# Deskripsi Pekerjaan
+- Membangun API Backend
+- Membangun Web Apps Pembayaran & Affiliator
+- Membangun Web Apps Monitoring
+- Membangun Mobile Apps iOS & Android
+- Deploy & Update Apps via AppStore/PlayStore
+- Integrasi vendor API channel
+- Membangun & Maintenance Website Company`,contract:o.FullTime,type:"Software Development",location:"Badung, Bali",period:{from:new Date(2020,2,1)},skills:n("js","php","vue","laravel","python","fastapi","flutter","dart","aws"),name:"Full-Stack Developer",color:"red",links:[],logo:e.awanbisnis,shortDescription:"Building, Maintenance and Deploying."},{slug:"software-freelance",company:"Self-employed",description:"Creating awesome applications for customers.",contract:o.Freelance,type:"Software Development",location:"Home",period:{from:new Date(2020,2,1)},skills:n(),name:"Freelancer",color:"blue",links:[],logo:e.Unknown,shortDescription:"Creating awesome applications for customers."}],s="Experience";export{i,s as t};
